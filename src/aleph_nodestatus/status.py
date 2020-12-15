f8ebdccf185761a281b9eaae12b0475faec82c5b
Jonathan Schemoul <jonathan.schemoul@gmail.com>
2020-12-15 22:27:07 +0100

    refactor: move to a class

--- src/aleph_nodestatus/status.py
+++ src/aleph_nodestatus/status.py
@@ -1,7 +1,8 @@
 from .erc20 import process_contract_history, DECIMALS
-from .messages import process_message_history
+from .messages import process_message_history, get_aleph_account, set_status
 from .settings import settings
 from .utils import merge
+import asyncio
 
 
 NODE_AMT = settings.node_threshold * DECIMALS
@@ -13,31 +14,140 @@ async def prepare_items(item_type, iterator):
         yield (height, (item_type, item))
 
 
-async def update_node_stats(balances, node_info):
-    node_info['stakers'] = {addr: balances[addr]
-                            for addr in node_info['stakers'].keys()}
-    node_info['total_staked'] = sum(node_info['stakers'].values())
-    return node_info
+class NodesStatus:
+    def __init__(self, initial_height=0):
+        self.nodes = {}
+        self.address_nodes = {}
+        self.address_staking = {}
+        self.balances = {}
+        self.last_checked_height = initial_height
 
+    async def update_node_stats(self, node_hash):
+        node_info = self.nodes[node_hash]
+        node_info['stakers'] = {addr: self.balances[addr]
+                                for addr in node_info['stakers'].keys()}
+        node_info['total_staked'] = sum(node_info['stakers'].values())
 
-async def remove_node(node_hash, address_nodes, address_staking, nodes):
-    node = nodes[node_hash]
-    for staker in node['stakers'].keys():
-        del address_staking[staker]
-    address_nodes.pop(node['owner'])
-    del nodes[node_hash]
+    async def remove_node(self, node_hash):
+        node = self.nodes[node_hash]
+        for staker in node['stakers'].keys():
+            del self.address_staking[staker]
+        self.address_nodes.pop(node['owner'])
+        del self.nodes[node_hash]
 
+    async def remove_stake(self, staker):
+        node_hash = self.address_staking[staker]
+        node = self.nodes[node_hash]
+        if staker in node['stakers']:
+            node['stakers'].pop(staker)
+        await self.update_node_stats(node_hash)
+        self.address_staking.pop(staker)
 
-async def remove_stake(staker, address_staking, nodes, balances):
-    node_hash = address_staking[staker]
-    node = nodes[node_hash]
-    if staker in node['stakers']:
-        node['stakers'].pop(staker)
-    await update_node_stats(balances, node)
-    address_staking.pop(staker)
+    async def process(self, iterators):
+        async for height, (evt_type, content) in merge(*iterators):
+            changed = True
+            if evt_type == 'balance-update':
+                balances, changed_addresses = content
+                for addr in changed_addresses:
+                    if addr in self.address_nodes:
+                        if balances[addr] < NODE_AMT:
+                            print(f"{addr}: should delete that node "
+                                  f"({balances[addr]/DECIMALS}).")
+
+                            await self.remove_node(self.address_nodes[addr])
+
+                    elif addr in self.address_staking:
+                        if balances[addr] < STAKING_AMT:
+                            print(f"{addr}: should kill its stake "
+                                  f"({balances[addr]/DECIMALS}).")
+                            await self.remove_stake(addr)
+                        else:
+                            await self.update_node_stats(
+                                self.address_staking[addr])
+
+                    else:
+                        changed = False
+
+            elif evt_type == 'staking-update':
+                message_content = content['content']
+                post_type = message_content['type']
+                post_content = message_content['content']
+                post_action = post_content['action']
+                address = message_content['address']
+
+                print(height,
+                      post_type,
+                      content['content']['address'],
+                      content['content']['content'])
+
+                existing_node = self.address_nodes.get(address, None)
+                existing_staking = self.address_staking.get(address, None)
+                ref = message_content.get('ref', None)
+
+                if post_type == settings.node_post_type:
+                    # Ok it's a wannabe node.
+                    # Ignore creation if there is a node already or imbalance
+                    if (post_action == "create-node"
+                            and address not in self.address_nodes
+                            and balances[address] > NODE_AMT):
+                        details = post_content.get('details', {})
+                        new_node = {
+                            'hash': content['item_hash'],
+                            'owner': address,
+                            'reward': details.get('reward_address', address),
+                            'name': details.get('name', ''),
+                            'multiaddress': details.get('multiaddress', ''),
+                            'stakers': {},
+                            'total_staked': 0,
+                            'status': 'waiting'
+                        }
+                        self.address_nodes[address] = content['item_hash']
+                        self.nodes[content['item_hash']] = new_node
+                        if address in self.address_staking:
+                            # remove any existing stake
+                            await self.remove_stake(address)
+
+                    elif (post_action == "drop-node"
+                          and address in self.address_nodes):
+                        if ref is not None and ref in self.nodes:
+                            if ref == existing_node:
+                                await self.remove_node(ref)
+
+                    else:
+                        print("This message wasn't registered (invalid)")
+                        changed = False
+
+                elif post_type == settings.staker_post_type:
+                    if (post_action == "stake"
+                            and balances[address] > STAKING_AMT
+                            and ref is not None and ref in self.nodes):
+                        if address in self.address_staking:
+                            # remove any existing stake
+                            await self.remove_stake(address)
+                        self.nodes[ref][address] = balances[address]
+                        self.address_staking[address] = ref
+                        await self.update_node_stats(ref)
+
+                    elif (post_action == "unstake"
+                          and address in self.address_staking
+                          and ref is not None
+                          and existing_staking == ref):
+                        await self.remove_stake(address)
+
+            if changed:
+                print(height, self.nodes, self.balances)
+                yield(height, self.nodes)
+
+            if height > self.last_checked_height:
+                self.last_checked_height = height
+            
+        # yield(self.last_checked_height, self.nodes)
 
 
 async def process():
+    state_machine = NodesStatus()
+    account = get_aleph_account()
+
     iterators = [
         prepare_items('balance-update', process_contract_history(
             settings.ethereum_token_contract, settings.ethereum_min_height)),
@@ -47,104 +157,41 @@ async def process():
              settings.staker_post_type],
             settings.aleph_api_server))
     ]
-
-    # Initialize the state machine
-    nodes = {}
-    address_nodes = {}
-    address_staking = {}
-    balances = {}
-
-    async for height, (evt_type, content) in merge(*iterators):
-        changed = True
-        if evt_type == 'balance-update':
-            balances, changed_addresses = content
-            for addr in changed_addresses:
-                if addr in address_nodes:
-                    if balances[addr] < NODE_AMT:
-                        print(f"{addr}: should delete that node "
-                              f"({balances[addr]/DECIMALS}).")
-
-                        await remove_node(address_nodes[addr], address_nodes,
-                                          address_staking, nodes)
-
-                elif addr in address_staking:
-                    if balances[addr] < STAKING_AMT:
-                        print(f"{addr}: should kill its stake "
-                              f"({balances[addr]/DECIMALS}).")
-                        await remove_stake(addr, address_staking,
-                                           nodes, balances)
-                    else:
-                        await update_node_stats(balances,
-                                                nodes[address_staking[addr]])
-                        
-                else:
-                    changed = False
-
-        elif evt_type == 'staking-update':
-            message_content = content['content']
-            post_type = message_content['type']
-            post_content = message_content['content']
-            post_action = post_content['action']
-            address = message_content['address']
-
-            print(height,
-                  post_type,
-                  content['content']['address'],
-                  content['content']['content'])
-
-            existing_node = address_nodes.get(address, None)
-            existing_staking = address_staking.get(address, None)
-            ref = message_content.get('ref', None)
-            
-            if post_type == settings.node_post_type:
-                # Ok it's a wannabe node.
-                # Ignore creation if there is a node already or if imbalance
-                if (post_action == "create-node"
-                        and address not in address_nodes
-                        and balances[address] > NODE_AMT):
-                    details = post_content.get('details', {})
-                    new_node = {
-                        'hash': content['item_hash'],
-                        'owner': address,
-                        'reward': details.get('reward_address', address),
-                        'name': details.get('name', ''),
-                        'multiaddress': details.get('multiaddress', ''),
-                        'stakers': {},
-                        'total_staked': 0,
-                        'status': 'waiting'
-                    }
-                    address_nodes = content['item_hash']
-                    nodes[content['item_hash']] = new_node
-                    if address in address_staking:
-                        # remove any existing stake
-                        await remove_stake(address, address_staking,
-                                           nodes, balances)
-                        
-                elif (post_action == "drop-node"
-                      and address in address_nodes):
-                    if ref is not None and ref in nodes:
-                        if ref == existing_node:
-                            await remove_node(ref, address_nodes,
-                                              address_staking, nodes)
-
-            elif post_type == settings.staker_post_type:
-                if (post_action == "stake"
-                        and balances[address] > STAKING_AMT
-                        and ref is not None and ref in nodes):
-                    if address in address_staking:
-                        # remove any existing stake
-                        await remove_stake(address, address_staking,
-                                           nodes, balances)
-                    nodes[ref][address] = balances[address]
-                    address_staking[address] = ref
-                    await update_node_stats(balances, nodes[ref])
-                    
-                elif (post_action == "unstake"
-                      and address in address_staking
-                      and ref is not None
-                      and existing_staking == ref):
-                    await remove_stake(address, address_staking,
-                                       nodes, balances)
-        if changed:
-            print(height, nodes)
+    nodes = None
+    async for height, nodes in state_machine.process(iterators):
+        pass
     
+    print("should set status")
+    await set_status(account, nodes)
+
+    i = 0
+    while True:
+        i += 1
+        last_height = state_machine.last_checked_height+1
+        iterators = [
+            prepare_items('staking-update',
+                          process_message_history(
+                              [settings.filter_tag],
+                              [settings.node_post_type,
+                               settings.staker_post_type],
+                              settings.aleph_api_server,
+                              min_height=last_height,
+                              request_count=1000,
+                              request_sort='-1'))
+        ]
+        if not i % 10:
+            iterators.append(
+                prepare_items('balance-update',
+                              process_contract_history(
+                                  settings.ethereum_token_contract,
+                                  last_height)))
+        nodes = None
+        async for height, nodes in state_machine.process(iterators):
+            pass
+
+        if nodes is not None:
+            await set_status(account, nodes)
+            print("should set status")
+
+        await asyncio.sleep(1)
+        
