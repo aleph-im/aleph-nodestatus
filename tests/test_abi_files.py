5e51055a5e4145976b49013893c207195230266c
amalcaraz <amalcaraz89@gmail.com>
2026-05-07 20:04:00 +0200

    chore(abi): vendor Uniswap V3 QuoterV2 ABI

--- tests/test_abi_files.py
+++ tests/test_abi_files.py
@@ -20,3 +20,12 @@ def test_payment_processor_abi_has_process_function():
         ("_amountOutMinimum", "uint128"),
         ("_ttl", "uint48"),
     ]
+
+
+def test_quoter_v2_abi_has_quote_exact_input():
+    abi = json.loads(_abi_path("UniswapV3QuoterV2").read_text())
+    fns = [item for item in abi if item.get("type") == "function"
+                                and item.get("name") == "quoteExactInput"]
+    assert len(fns) >= 1
+    inputs = [(i["name"], i["type"]) for i in fns[0]["inputs"]]
+    assert inputs == [("path", "bytes"), ("amountIn", "uint256")]
